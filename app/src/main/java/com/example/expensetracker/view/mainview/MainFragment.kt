06c6fa5e33d7f821b927ac09ae31f9ad41441956
Rayhan <mdrayhan8045@gmail.com>
2026-03-14 13:30:30 +0600

    menu bar

--- app/src/main/java/com/example/expensetracker/view/mainview/MainFragment.kt
+++ app/src/main/java/com/example/expensetracker/view/mainview/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.expensetracker.R
 import com.example.expensetracker.databinding.FragmentMainBinding
 import com.example.expensetracker.view.basefrag.Basefragment
@@ -17,6 +18,18 @@ class MainFragment : Basefragment<FragmentMainBinding>(
 
 ) {
     override fun createuser() {
+
+        with(binding){
+
+            clickBtn.setOnClickListener {
+
+                findNavController().navigate(R.id.action_mainFragment_to_useruploadFragment)
+
+            }
+
+
+
+        }
 
     }
 
--- app/src/main/java/com/example/expensetracker/view/mainview/UseruploadFragment.kt
+++ app/src/main/java/com/example/expensetracker/view/mainview/UseruploadFragment.kt
@@ -1,9 +1,13 @@
 package com.example.expensetracker.view.mainview
-
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.example.expensetracker.R
 import com.example.expensetracker.databinding.FragmentUseruploadBinding
@@ -19,7 +23,29 @@ class UseruploadFragment : Basefragment<FragmentUseruploadBinding>(
 
     override fun createuser() {
 
+        requireActivity().addMenuProvider(object : MenuProvider {
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+
+                return when(menuItem.itemId){
+
+                    R.id.mnu_expense -> {
+                        Toast.makeText(requireContext(),"Add Click",Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+        }, viewLifecycleOwner)
+
     }
+
 
 
 }
