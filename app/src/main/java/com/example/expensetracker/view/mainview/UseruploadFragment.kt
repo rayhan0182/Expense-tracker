8f55fe6df6005faf48e8d2da6e3bc294c6a7dc6c
Rayhan <mdrayhan8045@gmail.com>
2026-03-15 14:26:04 +0600

    Alert dialoge bar

--- app/src/main/java/com/example/expensetracker/view/mainview/UseruploadFragment.kt
+++ app/src/main/java/com/example/expensetracker/view/mainview/UseruploadFragment.kt
@@ -1,4 +1,5 @@
 package com.example.expensetracker.view.mainview
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -6,9 +7,13 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.expensetracker.R
 import com.example.expensetracker.databinding.FragmentUseruploadBinding
 import com.example.expensetracker.view.basefrag.Basefragment
@@ -23,29 +28,66 @@ class UseruploadFragment : Basefragment<FragmentUseruploadBinding>(
 
     override fun createuser() {
 
-        requireActivity().addMenuProvider(object : MenuProvider {
 
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.main_menu, menu)
-            }
+       toplevelmenubar()
 
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
 
-                return when(menuItem.itemId){
-
-                    R.id.mnu_expense -> {
-                        Toast.makeText(requireContext(),"Add Click",Toast.LENGTH_SHORT).show()
-                        true
-                    }
-
-                    else -> false
-                }
-            }
-
-        }, viewLifecycleOwner)
 
     }
 
+    private fun toplevelmenubar() {
+
+        binding.topToolbar.setOnMenuItemClickListener(){menuinflate->
+
+            when(menuinflate.itemId){
+
+                R.id.menu_income->{
+
+                    showincomedialoge()
+                    true
+                }
+
+                R.id.menu_expense->{
+
+                    Toast.makeText(requireContext(),"hello expense", Toast.LENGTH_LONG).show()
+                    true
+                }
+
+                else -> false
+
+            }
+
+        }
+
+    }
+
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
+    private fun showincomedialoge() {
+
+        val dialogeview = layoutInflater.inflate(R.layout.enteruserincome,null)
+
+
+        val dialoge = android.app.AlertDialog.Builder(requireContext())
+            .setView(dialogeview)
+            .create()
+
+        val e_text = dialogeview.findViewById<EditText>(R.id.et_amount)
+
+        val btn = dialogeview.findViewById<Button>(R.id.click_btn)
+
+
+          btn.setOnClickListener {
+
+              val income = e_text.text.toString()
+
+              Toast.makeText(requireContext(),"Income: $income",Toast.LENGTH_SHORT).show()
+
+              dialoge.dismiss()
+          }
+
+        dialoge.show()
+
+    }
 
 
 }
